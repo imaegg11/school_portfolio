342bd15776b9b9bbe6358bdcb8b48eb7b737b50d
imaegg11 <alex.yan3@student.tdsb.on.ca>
2024-03-05 22:46:46 -0500

    Code Blocks?!?!?!

--- OOP.js
+++ OOP.js
@@ -4,9 +4,15 @@ let add_numbers = async () => {
     for (let i = 0; i < code_blocks.length; i++) {
         let e = code_blocks[i];
         e.classList.add("hljs");
-        let new_text = e.innerText.split("\n").map((line, index) => {return `  ${index+1}. ${line}`});
+        let new_text = e.innerText.split("\n").map((line, index) => {return `${index+1}.   ${line}`});
         let obj = await hljs.highlight("java", new_text.join("\n"));
         e.innerHTML = obj.value;
+    }
+    let numbers = document.getElementsByClassName("hljs-number");
+    for (let number of numbers) {
+        if (/^\d+\.$/.test(number.innerText)) {
+            number.classList.add("number");
+        }
     }
 }
 // /^\d+\.$/
