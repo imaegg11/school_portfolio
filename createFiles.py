4d8d4b87c37761d6f9e597982068c2ae26f9bc48
imaegg11 <alex.yan3@student.tdsb.on.ca>
2024-03-17 20:17:52 -0400

    Code Works!!

---
+++ createFiles.py
@@ -0,0 +1,32 @@
+import shutil, os
+
+files = ["OOP.html", "Array.html", "Arraylists.html", "Searching.html", "Sorting.html", "Recursion.html"]
+ignoreList = ["index.html"]
+
+templateSource = "code\\template.html"
+
+def removeFiles():
+    print("Removing files...\n")
+    for file in os.listdir("."):
+        if file.endswith(".html") and file not in ignoreList:
+            print("Removed " + file)
+            os.remove(file);
+
+    print("Finished removing files...")
+
+def createFiles():
+    print("Creating new files...\n")
+
+    for file in files:
+        shutil.copyfile(templateSource, file)
+        print("Created " + file)
+
+    print("Finished creating files...")
+    print("Finished operation")
+
+removeFiles()
+createFiles()
+
+
+
+
