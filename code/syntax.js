c97d57a974c1cbbad2dcd730185049e9f1530312
imaegg11 <95944377+imaegg11@users.noreply.github.com>
2024-04-19 13:47:33 -0400

    Update syntax.js

--- code/syntax.js
+++ code/syntax.js
@@ -1,14 +1,16 @@
-let keywords = ["System", "abstract", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "synchronized", "boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw", "byte", "else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", "void", "class", "finally", "long", "strictfp", "volatile", "const", "float", "native", "super", "while"]
-let global_data_types = ["byte", "short", "int", "long", "float", "double", "boolean", "char", "String", "ArrayList", "Integer", "Boolean", "Double", "Character", "Byte", "Short", "Long", "Float"]
+let keywords = ["System", "abstract", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "synchronized", "boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw", "byte", "else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", "void", "class", "finally", "long", "strictfp", "volatile", "const", "float", "native", "super", "while"];
+let global_data_types = ["byte", "short", "int", "long", "float", "double", "boolean", "char", "String", "ArrayList", "Integer", "Boolean", "Double", "Character", "Byte", "Short", "Long", "Float"];
 
 let find_data_types = (text) => {
     let extended_classes = ([...text.matchAll(/extends\s+[a-zA-Z0-9]+/gm)]).map((e) => e[0].split(/ +/)[1]);
     let created_classes = ([...text.matchAll(/class\s+[a-zA-Z0-9]+/gm)]).map((e) => e[0].split(/ +/)[1]);
     let variable_data_types = ([...text.matchAll(/[a-zA-Z0-9]+[ ]+[a-zA-Z0-9]+[ ]+=[^=]/gm)]).map((e) => e[0].split(/[ ]+/)[0]);
-    return extended_classes.concat(created_classes, variable_data_types);
+    let implemented_interfaces = ([...text.matchAll(/implements([ ]+[a-zA-Z0-9]+[ ]?,?)+/gm)])[0][0].split(/[ /,]+/).slice(1);
+    let created_interfaces = ([...text.matchAll(/interface\s+[a-zA-Z0-9]+/gm)]).map((e) => e[0].split(/ +/)[1]);
+    return extended_classes.concat(created_classes, variable_data_types, implemented_interfaces, created_interfaces);
 }
 
-let parseToken = (token, isFunction, data_types) => {
+let parseToken = (token, isFunction, data_types, isVariable) => {
     if (keywords.includes(token)) {
         return "<span class='keyword'>" + token + "</span>";
     } else if (token.substring(0, 2) == "//") {
@@ -21,32 +23,53 @@ let parseToken = (token, isFunction, data_types) => {
         return "<span class='dataType'>" + token + "</span>";
     } else if (isFunction) {
         return "<span class='function'>" + token + "</span>";
+    } else if (isVariable) {
+        return "<span class='variable'>" + token + "</span>";
     } else {
         return token;
     }
 }
 
 let parseText = (text) => {
-    console.log(text);
-    let seperators = Array.from(";()[]{}<> ,:=!*&%.\n");
+    let seperators = Array.from(";()[]{}<>,:=!*&%.\n+ ");
     let currentToken = "";
     let highlightedText = "";
     let dataTypes = find_data_types(text).concat(global_data_types);
-    for (let char of text) {
+    for (let i = 0; i < text.length; i++) {
+        let char = text[i];
         if (seperators.includes(char)) {
             let tokenLength = currentToken.length;
             if (currentToken.substring(0, 2) == "//" && char != "\n") {
                 currentToken += char;
             } else if (Array.from("\"'").includes(currentToken[0]) && (currentToken[tokenLength - 1] != currentToken[0] || currentToken[tokenLength - 2] == "\\")) {
                 currentToken += char;
-            } else if (char == "(") {
-                highlightedText += parseToken(currentToken, true, dataTypes) + char;
-                currentToken = "";
             } else if (char == "." && !isNaN(currentToken)) {
                 currentToken += char;
+            } else if (char == " ") {
+                let capture = "";
+                while (text[i] == " ") {
+                  i++;
+                  capture += text[i];
+                }
+                if (capture[capture.length-1] == "(") {
+                  highlightedText += parseToken(currentToken, true, dataTypes) + capture.substring(0, capture.length);
+                  currentToken = "";
+                } else {
+                  i -= capture.length;
+                  // GG I made an accidental variable detector lmfao that only detects 
+                  // it if its being declared, or being called but not when there's a method
+                  // being called??? I have no idea how this works (I think it's something
+                  // to do with highlighting everyhting else and not actually a variable
+                  // detector)
+                  highlightedText += parseToken(currentToken, false, dataTypes, true) + char; 
+                  currentToken = "";
+                }
+            } else if (char == "(") {
+                  highlightedText += parseToken(currentToken, true, dataTypes) + char;
+                  currentToken = "";
             } else {
-                highlightedText += parseToken(currentToken, false, dataTypes) + char;
-                currentToken = "";
+                  highlightedText += parseToken(currentToken, false, dataTypes) + char;
+                  currentToken = "";
             }
         } else {
             currentToken += char;
