5d6a2def6db88825c817cae9a2d2e4365d415536
imaegg11 <alex.yan3@student.tdsb.on.ca>
2024-04-21 12:57:01 -0400

    quickFix

--- code/syntax.js
+++ code/syntax.js
@@ -5,7 +5,8 @@ let find_data_types = (text) => {
     let extended_classes = ([...text.matchAll(/extends\s+[a-zA-Z0-9]+/gm)]).map((e) => e[0].split(/ +/)[1]);
     let created_classes = ([...text.matchAll(/class\s+[a-zA-Z0-9]+/gm)]).map((e) => e[0].split(/ +/)[1]);
     let variable_data_types = ([...text.matchAll(/[a-zA-Z0-9]+[ ]+[a-zA-Z0-9]+[ ]+=[^=]/gm)]).map((e) => e[0].split(/[ ]+/)[0]);
-    let implemented_interfaces = ([...text.matchAll(/implements([ ]+[a-zA-Z0-9]+[ ]?,?)+/gm)])[0][0].split(/[ /,]+/).slice(1);
+    console.log([...text.matchAll(/implements[ ]+([a-zA-Z0-9]+[ ]*,?[ ]*)*/gm)])
+    let implemented_interfaces = [...text.matchAll(/implements[ ]+([a-zA-Z0-9]+[ ]*,?[ ]*)*/gm)].length != 0 ? ([...text.matchAll(/implements[ ]+([a-zA-Z0-9]+[ ]*,?[ ]*)*/gm)])[0][0].split(/[ /,]+/).slice(1) : [];
     let created_interfaces = ([...text.matchAll(/interface\s+[a-zA-Z0-9]+/gm)]).map((e) => e[0].split(/ +/)[1]);
     return extended_classes.concat(created_classes, variable_data_types, implemented_interfaces, created_interfaces);
 }
@@ -61,7 +62,7 @@ let parseText = (text) => {
                   // being called??? I have no idea how this works (I think it's something
                   // to do with highlighting everyhting else and not actually a variable
                   // detector)
-                  highlightedText += parseToken(currentToken, false, dataTypes, true) + char; 
+                  highlightedText += parseToken(currentToken, false, dataTypes) + char; 
                   currentToken = "";
                 }
             } else if (char == "(") {
